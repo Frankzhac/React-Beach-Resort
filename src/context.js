b8e7068c05e2fcad57baa6b69f7b8d772a437b99
Frankzhac <Frank.Hemle@gmail.com>
2019-05-16 10:19:06 -0400

    React.createContext Api implemented to context component

---
+++ src/context.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+
+const RoomContext = React.createContext();
+// <RoomContext.Provider value={"Hello"}
+
+class RoomProvider extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <RoomContext.Provider value="Hello">
+        {this.props.children}
+      </RoomContext.Provider>
+    )
+  }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export{ RoomProvider, RoomConsumer, RoomContext };
